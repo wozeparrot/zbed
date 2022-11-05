3728c9d0af7798376e5ffa3054b27e5568ca5544
Woze Parrot <wozeparrot@gmail.com>
2022-11-05 15:40:57 -0400

    feat: changes

--- pkg.zig
+++ pkg.zig
@@ -1,22 +1,24 @@
 const std = @import("std");
 
-pub fn Pkg(path: comptime []const u8) type {
+pub fn Pkg(path: []const u8) type {
     return struct {
         var zbed = std.build.Pkg{
             .name = "zbed",
-            .path = path ++ "/src/zbed.zig",
+            .path = .{ .path = path ++ "/src/zbed.zig" },
         };
 
         var wrapper = std.build.Pkg{
             .name = "zbed_wrapper",
-            .path = path ++ "/src/wrapper.zig",
+            .path = .{ .path = path ++ "/src/wrapper.zig" },
         };
 
         /// Adds zbed to a step
         pub fn addTo(b: *std.build.Builder, step: *std.build.LibExeObjStep, io_target: []const u8) void {
+            _ = b;
+
             zbed.dependencies = &[_]std.build.Pkg{.{
                 .name = "build_options",
-                .path = std.mem.concat(step.builder.allocator, u8, &[_][]const u8{ "zig-cache/", step.name, "_build_options.zig" }) catch unreachable,
+                .path = .{ .path = std.mem.concat(step.builder.allocator, u8, &[_][]const u8{ "zig-cache/", step.name, "_build_options.zig" }) catch unreachable },
             }};
             wrapper.dependencies = &[_]std.build.Pkg{zbed};
             step.addPackage(zbed);
